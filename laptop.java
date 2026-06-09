b7e464d7dac16932777681b8f6e682da642b0a2d
Mysticalgamer38 <edwin.granados@campus.mccd.edu>
2026-06-09 16:11:10 -0700

    Create laptop.java

---
+++ laptop.java
@@ -0,0 +1,12 @@
+public class Laptop {
+
+    String brand;
+    int ramGB;
+    double screenSize;
+
+    public Laptop(String brand, int ramGB, double screenSize) {
+        this.brand = brand;
+        this.ramGB = ramGB;
+        this.screenSize = screenSize;
+    }
+}
