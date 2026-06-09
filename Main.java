58cb1082d5d2c747bf13571971733cf182a23ff8
Mysticalgamer38 <edwin.granados@campus.mccd.edu>
2026-06-09 14:41:10 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,6 @@
+// A1: Create a Dog with no constructors defined.
+Dog dog1 = new Dog();
 
+// A2: Print the object directly.
+System.out.println("Printing dog1 directly:");
+System.out.println(dog1);
